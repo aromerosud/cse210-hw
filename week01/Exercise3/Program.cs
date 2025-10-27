2494028c4b480f87d8bdc434fac059e12ef7f084
Arturo Romero <romerosud89@gmail.com>
2025-10-27 13:52:23 -0500

    Exercise 3 - Loops

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+        int count = 0;
+        string has_again = "yes";
+
+        while (has_again.ToLower().Equals("yes")){
+            Console.Write("What is your guess? ");
+            int guess = int.Parse(Console.ReadLine());
+            if (guess < number)
+            {
+                Console.WriteLine("Higher");
+                count = count + 1;
+            }
+            else if (guess > number)
+            {
+                Console.WriteLine("Lower");
+                count = count + 1;
+            }
+            else
+            {
+                count = count + 1;
+                Console.WriteLine("You guessed it!");
+                Console.WriteLine($"It look yoy {count} guesses");
+                Console.WriteLine("");
+                Console.WriteLine("Would you like to play again (yes/no)? ");
+                has_again = Console.ReadLine();
+                number = randomGenerator.Next(1, 100);
+                count = 0;
+            }
+        }
+
+        Console.WriteLine("Thank you for playing. Goodbye.");
     }
 }
