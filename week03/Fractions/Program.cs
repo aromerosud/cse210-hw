b2b0cd48d9d2cf8c29cd0a212c9dd1d6f13b7d1f
Arturo Romero <romerosud89@gmail.com>
2025-11-07 22:48:26 -0500

    W03 Learning Activity: Encapsulation

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction fraction = new Fraction();
+        Fraction fraction1 = new Fraction(5);
+        Fraction fraction2 = new Fraction(6, 7);        
+        
+
+        fraction2.SetTop(3);
+        fraction2.SetBottom(4);
+
+        Console.WriteLine(fraction.GetFractionString());
+        Console.WriteLine(fraction.GetDecimalValue());
+        Console.WriteLine(fraction1.GetFractionString());
+        Console.WriteLine(fraction1.GetDecimalValue());
+        Console.WriteLine(fraction2.GetFractionString());
+        Console.WriteLine(fraction2.GetDecimalValue());
+
+        fraction2.SetTop(1);
+        fraction2.SetBottom(3);
+
+        Console.WriteLine(fraction2.GetFractionString());
+        Console.WriteLine(fraction2.GetDecimalValue());
+
     }
 }
