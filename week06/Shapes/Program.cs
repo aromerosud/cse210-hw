ecf049a21665904638205fb108f2b34502257fd6
Arturo Romero <romerosud89@gmail.com>
2025-12-01 19:24:53 -0500

    W06 Learning Activity: Polymorphism

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square("blue", 5));
+        shapes.Add(new Rectangle("Green", 5, 3));
+        shapes.Add(new Circle("Red", 8));
+
+        foreach(Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
